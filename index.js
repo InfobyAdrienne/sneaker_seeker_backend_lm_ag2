2cd5989bb6548ecd773bd1979c4f01f29a8d7107
LauraMaddaluno <lauralondon.4@gmail.com>
2021-11-09 19:32:51 +0000

    remove port 80

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ app.use(cors())
 app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
- 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
 
 app.get('/api', (incomingRequest, outgoingResponse) => {
     console.log(incomingRequest.query)
