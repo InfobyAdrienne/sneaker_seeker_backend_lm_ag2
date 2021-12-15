7c340c029ef3c1133653679cc8a2fe81b4723c0a
LauraMaddaluno <lauralondon.4@gmail.com>
2021-12-15 13:13:20 +0000

    filter for null image and add function generateUnirestRequest to DRY code

--- index.js
+++ index.js
@@ -1,101 +1,67 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3001
-var unirest = require("unirest")
-var cors = require('cors')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+var unirest = require("unirest");
+var cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
-// app.get("/api/:id", function (req, res, next) {
-//   try {
-//     // assume you fetch from the database which contains null values here
-//     const withNullImg = generateUnirestRequest(
-//       `https://v1-sneakers.p.rapidapi.com/v1/sneakers/${incomingRequest.params.id}`
-//     );
+app.get("/api", (incomingRequest, outgoingResponse) => {
+  var rapidApiRequest = generateUnirestRequest(
+    "https://v1-sneakers.p.rapidapi.com/v1/sneakers"
+  );
+  let filters = {
+    limit: "40",
+  };
+  if (incomingRequest.query.brand !== undefined) {
+    filters.brand = incomingRequest.query.brand;
+  }
+  if (incomingRequest.query.limit !== undefined) {
+    filters.limit = incomingRequest.query.limit;
+  }
+  if (incomingRequest.query.gender !== undefined) {
+    filters.gender = incomingRequest.query.gender;
+  }
+  if (incomingRequest.query.colorway !== undefined) {
+    filters.colorway = incomingRequest.query.colorway;
+  }
+  rapidApiRequest.query(filters);
 
-//     const ImageResponse = sneaker.media.thumbUrl.filter((item) => {
-//       let isValidObject = true;
-//       if (!item.img)
-//         // change this line according to what you want
-//         isValidObject = false;
-//       return isValidObject; // if true, means we want this object, false means filter this object
-//     });
-//   } catch (e) {
-//     //handle error
-//   }
-// });
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.get('/api', (incomingRequest, outgoingResponse) => {
-    console.log(incomingRequest.query)
-	
-
-  var rapidApiRequest = unirest("GET", "https://v1-sneakers.p.rapidapi.com/v1/sneakers");
-	let filters = {
-		"limit": "40"
-	}
-	if (incomingRequest.query.brand !== undefined) {
-		filters.brand = incomingRequest.query.brand;
-	}
-	if (incomingRequest.query.limit !== undefined) {
-		filters.limit = incomingRequest.query.limit;
-	}
-	if (incomingRequest.query.gender !== undefined) {
-		filters.gender = incomingRequest.query.gender;
-	}
-	if (incomingRequest.query.colorway!== undefined) {
-		filters.colorway = incomingRequest.query.colorway;
-	}   
-		rapidApiRequest.query(filters);
-
-	rapidApiRequest.headers({
-		"x-rapidapi-host": "v1-sneakers.p.rapidapi.com",
-		"x-rapidapi-key": process.env.RAPID_API_KEY,
-		"useQueryString": true,
-		"Access-Control-Allow-Origin": "http://localhost:3000/",
-		"Access-Control-Allow-Credentials": true
-	});
-
-	rapidApiRequest.end(function (rapidApiResponse) {
-		if (rapidApiResponse.error) throw new Error(rapidApiResponse.error);
-
-		
-
-		outgoingResponse.send(rapidApiResponse.body)
-	});
-
+  rapidApiRequest.end(function (rapidApiResponse) {
+    if (rapidApiResponse.error) throw new Error(rapidApiResponse.error);
+    let shoes_with_img = rapidApiResponse.body.results.filter(
+      (shoe) => shoe.media.thumbUrl !== null
+    );
+    let response = { results: shoes_with_img };
+    outgoingResponse.send(response);
+  });
 });
 
-app.get("/api/:id", (incomingRequest, outgoingResponse) => { 
-	var rapidApiRequest = unirest("GET", `https://v1-sneakers.p.rapidapi.com/v1/sneakers/${incomingRequest.params.id}`);
-	rapidApiRequest.headers({
-		"x-rapidapi-host": "v1-sneakers.p.rapidapi.com",
-		"x-rapidapi-key": process.env.RAPID_API_KEY,
-		"useQueryString": true,
-		"Access-Control-Allow-Origin": "http://localhost:3000/",
-		"Access-Control-Allow-Credentials": true
-	});
+app.get("/api/:id", (incomingRequest, outgoingResponse) => {
+  var rapidApiRequest = generateUnirestRequest(
+    `https://v1-sneakers.p.rapidapi.com/v1/sneakers/${incomingRequest.params.id}`
+  );
 
-	rapidApiRequest.end(function (rapidApiResponse) {
-		if (rapidApiResponse.error) throw new Error(rapidApiResponse.error);
-
-		console.log(rapidApiResponse.body);
-
-		outgoingResponse.send(rapidApiResponse.body)
-	});
-	
-}) 
+  rapidApiRequest.end(function (rapidApiResponse) {
+    if (rapidApiResponse.error) throw new Error(rapidApiResponse.error);
+    outgoingResponse.send(rapidApiResponse.body);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-
-
-
-
+function generateUnirestRequest(url) {
+  var rapidApiRequest = unirest("GET", url);
+  rapidApiRequest.headers({
+    "x-rapidapi-host": "v1-sneakers.p.rapidapi.com",
+    "x-rapidapi-key": process.env.RAPID_API_KEY,
+    useQueryString: true,
+    "Access-Control-Allow-Origin": "http://localhost:3000/",
+    "Access-Control-Allow-Credentials": true,
+  });
+  return rapidApiRequest;
+}
